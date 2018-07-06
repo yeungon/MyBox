a565e805a0f9c588c3ce4ab649414aaa692b4eb5
yeungon <yeungon@gmail.com>
2018-07-06 06:15:02 -0600

    request email controller

---
+++ app/Http/Controllers/Front/Home/HomeResetPasswordController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers\Front\Home;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon; /*thư viện thời gian
+https://scotch.io/tutorials/easier-datetime-in-laravel-and-php-with-carbon*/
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Mail;
+use Illuminate\Mail\Mailable;
+
+
+class HomeResetPasswordController extends Controller
+{	
+	/*Chỉ khi không đăng nhập mới được reset password*/
+	 public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+
+    public function showResetForm(Request $request, $token = null, $email = null)
+    {	
+	    if($token == null || $email == null){
+
+	    	return redirect()->route("home.index");
+	    }
+
+
+	    if ($token != null){
+
+	    	$reset = DB::table('password_resets')->where('email', $email)->first();
+
+	    	/*Lấy username*/
+
+	    	$username = DB::table('users')->where('email', $email)->first();
+
+		    /*Kiểm tra xem token hết hạn chưa*/
+
+		    /*Thời gian 1 tiếng trước*/
+		    $onehour = Carbon::now()->subHours(1);
+			
+
+		    if($reset->created_at < $onehour){
+		    	echo "Yoop! It appears that your token is expired already!";
+		    }
+
+		    /*Lấy token và hashed để đối chiếu với database, gợi ý
+			https://github.com/laravel/framework/issues/18570
+	    	*/
+
+		    /*Đối chiếu token trong database*/
+		    if (!Hash::check($token, $reset->token)) {
+    			
+    			echo "It appears that your token is incorrect";
+			}else{
+
+				/*Truyền username và email sang view => function reset để xử lý*/
+				$data['username'] = $username->username;
+
+				$data['email'] = $username->email;
+
+				return view("frontend.home.resetpassword", $data);
+			}
+
+		}else{
+
+			return redirect()->route("home.index");
+
+		}
+    	    	
+    }
+
+
+    public function reset(Request $request)
+    {
+
+    	$valid = Validator::make($request->all(), [
+            
+            'password' => 'required|string|min:5|max:50|confirmed',
+            
+                                 
+        ],[
+            /*Customeize the error*/
+            
+            'password.min' => 'The message should have more than 5 characters!',
+            'password.max' => 'The message should have less than 50 characters!',
+        ]);
+
+
+        if($valid->fails()){
+            
+            return redirect()->back()->withErrors($valid)->withInput();
+
+         }else{
+
+         	$username = $request->usernametoupdate;
+         	$password = $request->password;
+         	$email = $request->email;
+			
+			
+
+			/*test xem có thể gửi email
+			Gửi string email, dùng closure để truyền vào email là biến use($email)
+			https://laracasts.com/discuss/channels/laravel/send-raw-text-mail-without-using-a-view-in-laravel-53
+			*/
+
+			$content = "Hi $username! Your password has been changed! Cheer!";
+			
+			Mail::raw($content, function ($message) use($email, $username) {
+			   $message ->to($email);
+			   $message -> subject('Urgent! Your password has been changed!');
+			   $message -> from('myboxdotnz@gmail.com', 'Mybox.nz - Secured Box');
+			});
+
+
+			echo "đã chạy";
+
+			die(); //dừng tại đây để chỉnh tiếp
+
+
+			/*cập nhật password mới và xóa token trong reset*/         	
+			/*Update new password, dùng Eloquent*/
+			User::where('username', $username)->update(['password' => bcrypt($password)]);
+			/*Cập nhật bảng password_resets, xóa token, vì hem có Model thui em chơi DB :-), as we dont have a model for the table password_resets, then we have to use Query Builder*/
+			DB::table('password_resets')->where('email', $email)->update(['token' => "", 'created_at' => null]);
+
+			
+			// /*Gửi mail thông báo - Send a notification*/
+			// $a = new Mailable;
+   //  		Mail::to($email)->send("Your password has been changed!");
+
+
+    
+			
+			return redirect()->route("home.index")->with('message', "Your password has been successfully changed! ");
+
+
+         }
+
+    }
+
+   
+
+}
