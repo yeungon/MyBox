f059ec1c5ba2da8c8412cf4e942a39d72049e273
yeungon <yeungon@gmail.com>
2018-07-06 04:43:57 -0600

    change the name of the site

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'HashBox'),
+    'name' => env('APP_NAME', 'MyBox - Secured Box'),
 
     /*
     |--------------------------------------------------------------------------
